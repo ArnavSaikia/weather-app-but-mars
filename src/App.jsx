c5aba71f0bf6679d504a784fabad62799980e8bb
ArnavSaikia <arnavsaikia817@gmail.com>
2025-06-06 20:52:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import bgImage from './assets/background-temp.jpg';
+import Metrics from './Metrics.jsx'
+
+
+function App() {
+    const styles = {
+        backgroundImage: `url(${bgImage})`,
+        width: "100vw",
+        height: "100vh",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        display: "flexbox",
+        padding: "20px",
+        paddingLeft: "50px"
+    };
+
+
+
+
+    // const [data, setData] = useState(null);
+    // useEffect(() => {
+    //     fetch("https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0")
+    //     .then((returned) => returned.json())
+    //     .then(json => {
+    //         setData(json);
+    //     })
+    //     .catch(err => console.log(err));
+    // }, []);
+
+    // console.log(data);
+
+    return(
+        <>
+            <div style={styles} className="text-box">
+            <p className='title-insight'>
+                Latest Weather<br></br>at Elysium Planitia
+            </p>
+            <p className="description-text">
+                Daily weather measurements of the surface of Mars at Elysium Planitia <br></br>near Martian equator by InSight lander
+            </p>
+            <Metrics/>
+            </div >
+        </>  
+    )
+}
+
+export default App
---
+++ src/Metrics.jsx
@@ -0,0 +1,12 @@
+function Metrics(props){
+    return(
+        <div>
+            <div>
+                Sol 265<br></br>
+                August 25
+            </div>
+        </div>
+    )
+}
+
+export default Metrics;
